74cafd61de7ecb09307237cbb29b9cfd79411047
mateus <mateus46885@gmail.com>
2026-08-07 22:19:51 -0300

    consertando o popular_banco.py para o novo fluxo de senha hash

--- popular_banco.py
+++ popular_banco.py
@@ -1,22 +1,25 @@
 from main import app
 from models.models import db, Usuario
+from werkzeug.security import generate_password_hash
 
 with app.app_context():
-    # Verifica se o administrador já existe para evitar duplicados
-    admin_existente = Usuario.query.filter_by(login='admin').first()
+    # Cria o arquivo .db e todas as tabelas automaticamente se não existirem
+    db.create_all()
     
-    if not admin_existente:
-        # Criando o primeiro utilizador
-        primeiro_admin = Usuario(
-            nome='Administrador do Sistema',
-            login='admin',
-            senha='123', # Senha simples para teste inicial
-            tipo_acesso='Administrador'
-        )
-        
-        db.session.add(primeiro_admin)
-        db.session.commit()
-        print("Sucesso! Usuário 'admin' criado.")
-        print("Acesse com -> Login: admin | Senha: 123")
-    else:
-        print("O usuário 'admin' já existe no banco de dados.")
+    # Cria o administrador
+    primeiro_admin = Usuario(
+        nome='Administrador do Sistema',
+        login='admin',
+        senha=generate_password_hash('123'),
+        tipo_acesso='Administrador'
+    )
+    
+    # Guarda no banco de dados
+    db.session.add(primeiro_admin)
+    db.session.commit()
+    
+    print("======================================================")
+    print(" Sucesso! Banco de dados criado e tabelas geradas.")
+    print(" Utilizador 'admin' criado.")
+    print(" Acesse com: \nLogin: admin | Senha: 123")
+    print("======================================================")
